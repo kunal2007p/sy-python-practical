eb9c5f961d6312980338c9b9ebf7f3a082a0678f
kunal2007p <kunalpwar802@gmail.com>
2026-09-01 13:59:55 +0530

    Add files via upload

---
+++ prac5 EX2.py
@@ -0,0 +1,12 @@
+print("**************** Invoice Frame ****************")
+
+rows = 8
+cols = 30
+
+for i in range(rows):
+    for j in range(cols):
+        if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
