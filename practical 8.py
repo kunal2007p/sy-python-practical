dc280cd0e66c9eff507cb55930f255ed6925f3cf
kunal2007p <kunalpwar802@gmail.com>
2026-09-01 13:59:07 +0530

    Add files via upload

---
+++ practical 8.py
@@ -0,0 +1,10 @@
+feedback = input("Enter customer feedback: ")
+
+feedback = feedback.strip()
+feedback = feedback.capitalize()
+
+print("\n----- CUSTOMER FEEDBACK -----")
+print("Feedback:", feedback)
+print("Thank you for your valuable feedback!")
+print("-----------------------------")
+
