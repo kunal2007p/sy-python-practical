07e06b65ca83cbc1c75f7fae8303b10f6e51ac65
kunal2007p <kunalpwar802@gmail.com>
2026-09-01 13:59:37 +0530

    Add files via upload

---
+++ prac 5 ex1.py
@@ -0,0 +1,8 @@
+print("*********** Right Angle Triangle Pattern ***********")
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
